f3100793b48f3d150454bd160c1a256890d4e4f5
Oscar Munoz <oscaremu@gmail.com>
2018-05-20 18:15:16 -0500

    Added parsedown parse.

--- md.php
+++ md.php
@@ -10,42 +10,31 @@
  *
  * @uses		Wakka::HasAccess()
  */
- $loader = require 'vendor/autoload.php';
- $loader->add('AppName', __DIR__.'/../src/');
+# $loader = require 'vendor/autoload.php';
+# $loader->add('AppName', __DIR__.'/../src/');
+include_once 'parsedown/Parsedown.php';
 
 if ($this->HasAccess('read') && $this->page)
 {
-  // Make robust the way to find the css and the js. The handler can be on many directorys.
-  $relative_path = explode( ',', $this->GetConfigValue('handler_path'));
-
-  // If nothing works try with this very basic prism.
-  $prims_css_default = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.6.0/themes/prism.min.css";
-  $prims_js_default = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.6.0/prism.min.js";
-
-  // Check for the css and the js files. If exist overwrite the black/withe web.
-  foreach ($relative_path as $key => $value) {
-    if (is_file($relative_path[$key].'/md/prism.css') && is_file($relative_path[$key].'/md/prism.js'))
-    {
-      $prims_css_default = $relative_path[$key].'/md/prism.css';
-      $prims_js_default = $relative_path[$key].'/md/prism.js';
-    }
-  }
-
 	// display markdown page.
 
-  // TODO this options must be prefered on configuration file of the wikka or even database.
+#	$parsedown = new Parsedown;
+  // TODO this options must be prefered on configuration file.
 	// traditional markdown and parse full text
   //$parser = new \cebe\markdown\Markdown();
 	// use github markdown
-  $parser = new \cebe\markdown\GithubMarkdown();
+  #$parser = new \cebe\markdown\GithubMarkdown();
+#  $Parsedown = new Parsedown();
 	// use markdown extra
   //$parser = new \cebe\markdown\MarkdownExtra();
 
 	// TODO ask to use and put this on the header.php. (head html) Maybe this dont work on very old browers becose of this.
-	echo '<link rel="stylesheet" type="text/css" href="'.$prims_css_default.'" />';
-	echo '<script src="'.$prims_js_default.'"></script>';
+	echo '<link rel="stylesheet" type="text/css" href="'.$this->GetThemePath('/').'/css/prism.css" />';
+        echo '<link rel="stylesheet" type="text/css" href="'.$this->GetThemePath('/').'/css/mermaid.css" />';
+	echo '<script src="'.$this->GetThemePath('/').'/js/prism.js"></script>';
+	echo '<script src="'.$this->GetThemePath('/').'/js/mermaid.min.js"></script>';
 
-  // TODO This make "like" the wiki, this needs more (check for other missing css options and sets).
+  // TODO This make more "like" the wiki, this needs more (check for other missing options and sets).
 	echo "\n".'<!--starting page content-->'."\n";
 	echo '<div id="content"';
 	echo (($user = $this->GetUser()) && ($user['doubleclickedit'] == 'N') || !$this->HasAccess('write')) ? '' : ' ondblclick="document.location=\''.$this->Href('edit', '', 'id='.$this->page['id']).'\';" '; #268
@@ -53,8 +42,16 @@ if ($this->HasAccess('read') && $this->page)
 
 	// TODO Needs a TOC generetor.
 
-	echo $parser->parse($this->page['body']);
+	#echo $parser->text($this->page['body']);
+	#echo $parsedown->text('Hello _Parsedowhhhhn_!');
+	echo Parsedown::instance()->text($this->page['body']); 
 
+
+#	echo '<script>mermaid.initialize({startOnLoad:true});</script>';
+#        echo '<script>mermaid.init({noteMargin: 10}, ".language-mermaid");</script>';
+#	echo '<script>mermaid.init({cloneCssStyles: true}, ".language-mermaid");</script>';
+#	echo '<script>mermaid.init(undefined, $("#someId .language-mermaid"));</script>';
   echo '</div>';
+  echo '<script>mermaid.init({cloneCssStyles: true}, ".language-mermaid");</script>';
 }
 ?>
